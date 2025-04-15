aa9315dbe7c859c2d9d7cbb301687a05463124ec
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-04-15 14:36:39 +0200

    update

--- app.js
+++ app.js
@@ -5,13 +5,19 @@
 
 import express from 'express';
 import cors from 'cors'
+import morgan from "morgan";
 import 'dotenv/config'
 import './db/db.js'
 
 const app = express()
+app.use(morgan("tiny"));
 app.use(cors())
 app.use(express.json())
 
+// app.use('api/')
+// app.use((_, res)=>{
+
+// })
 app.listen(2060, ()=>{
     console.log("Server is running. Use our API on port: 2060")
 })
