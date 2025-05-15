37bf4438e9a27547e3c551acb1a7143c5dfbdebe
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-05-15 15:03:09 +0200

    update

--- db/db.js
+++ db/db.js
@@ -15,18 +15,17 @@ import mongoose from "mongoose";
 
 
 
-const env = process.env.NODE_ENV || 'development'
-const DB_HOST = env === 'test' ? process.env.DB_HOST : process.env.DB_TEST;
+const env = process.env.NODE_ENV || 'development';
+const DB_HOST = env === 'test' ? process.env.DB_TEST : process.env.DB_HOST;
 
-
-
-
+if(!DB_HOST){
+  console.error("MongoDB URI is not defined in environment variables")
+  process.exit(1);
+}
 mongoose
   .connect(DB_HOST)
-  .then(() => {
-    console.log("Data base connect success");
-  })
-  .catch((error) => {
-    console.error("Database connection error", error);
+  .then(() => console.log("Database connected"))
+  .catch((err) => {
+    console.error("Database connection error", err);
     process.exit(2);
   });
