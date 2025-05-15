cdd462c5f94b6e4d5cfa02e836060b0e1c85e13b
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-05-15 14:57:24 +0200

    add data base for swagger
    setting environment variables

--- db/db.js
+++ db/db.js
@@ -1,7 +1,25 @@
 import "dotenv/config";
 import mongoose from "mongoose";
 
-const DB_HOST = process.env.DB_HOST;
+// const DB_HOST = process.env.DB_HOST;
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     console.log("Data base connect success");
+//   })
+//   .catch((error) => {
+//     console.error("Database connection error", error);
+//     process.exit(2);
+//   });
+
+
+
+const env = process.env.NODE_ENV || 'development'
+const DB_HOST = env === 'test' ? process.env.DB_HOST : process.env.DB_TEST;
+
+
+
 
 mongoose
   .connect(DB_HOST)
