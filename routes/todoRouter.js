2d701f050bd511e72f20efd2ed426bcca21ee234
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-11-16 16:58:15 +0100

    chenge_schema

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { getAllTodo, getOneTodo, createTodo, updateTodo, deleteTodo } from "../controllers/todoControllers.js";
 import validatetBody from "../helpers/validateBody.js";
-import {createTodoSchema } from "../schemas/todoSchema.js"
+import {createTodoSchema, updateTodoSchema } from "../schemas/todoSchema.js"
 
 const todoRouter = express.Router();
 
 todoRouter.get("/", getAllTodo);
 todoRouter.get("/:id",getOneTodo);
 todoRouter.post("/",validatetBody(createTodoSchema),createTodo);
-todoRouter.patch("/:id",validatetBody(createTodoSchema),updateTodo)
+todoRouter.patch("/:id",validatetBody(updateTodoSchema),updateTodo)
 todoRouter.delete("/:id", deleteTodo )
 
 
