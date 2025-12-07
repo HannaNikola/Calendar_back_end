aa03054a8cf456a73e13de70b61939870478d811
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-12-07 20:46:11 +0100

    fix_bug

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -1,6 +1,5 @@
-import { userAgent } from "next/server.js";
+// import { userAgent } from "next/server.js";
 import httpError from "../helpers/httpError.js";
-import eventServices from "../services/eventServices.js";
 import todoServices from "../services/todoServices.js";
 
 
@@ -155,82 +154,3 @@ export const deleteTodo = async(req, res, next)=>{
 
 
 
-// export const getAllEvents = async (req, res, next) => {
-//   try {
-//     const { _id: userId } = req.user;
-//     const data = await eventServices.listEvents(userId);
-//     res.status(200).send(data);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
-// export const getOneEvent = async (req, res, next) => {
-//   try {
-//     const { _id: userId } = req.user;
-//     const { id } = req.params;
-
-//     const data = await eventServices.getEventById({
-//       idEvent: id,
-//       owner: userId
-//     });
-
-//     if (!data) throw httpError(404, "Event not found");
-
-//     res.status(200).send(data);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
-// export const createEvent = async (req, res, next) => {
-//   try {
-//     const { _id: userId } = req.user;
-
-//     const data = await eventServices.addEvent({
-//       ...req.body,
-//       owner: userId
-//     });
-
-//     res.status(201).send(data);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
-// export const deleteEvent = async (req, res, next) => {
-//   try {
-//     const { _id: userId } = req.user;
-//     const { id } = req.params;
-
-//     const data = await eventServices.removeEvent({ id, owner: userId });
-
-//     if (!data) throw httpError(404, "Event not found");
-
-//     res.status(200).json({
-//       message: "Success",
-//       data
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
-// export const updateEvent = async (req, res, next) => {
-//   try {
-//     const { _id: userId } = req.user;
-//     const { id } = req.params;
-
-//     const data = await eventServices.updateEventById({
-//       id,
-//       owner: userId,
-//       body: req.body
-//     });
-
-//     if (!data) throw httpError(404, "Event not found");
-
-//     res.status(200).send(data);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
