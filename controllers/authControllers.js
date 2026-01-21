1b6606377953d2cad94e32dbc2b0061ded5254f7
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2026-01-21 16:52:39 +0100

    verify-email

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -73,7 +73,9 @@ export const authRegister = async (req, res, next) => {
     <h2>Confirm your email</h2>
     <p>Please confirm your email address by clicking the button below:</p>
 
-    <a href="${process.env.FRONTEND_URL}/verify-email?token=${token}"
+    // <a href="${process.env.FRONTEND_URL}/verify-email?token=${token}"
+    <a href="${process.env.BACKEND_URL}/api/users/verify-email?token=${token}"
+
        style="
          display: inline-block;
          padding: 12px 24px;
@@ -97,6 +99,47 @@ export const authRegister = async (req, res, next) => {
     message: "Registration successful. Check your email.",
   });
 };
+
+// export const verifyEmail = async (req, res) => {
+//   const { token } = req.query;
+
+//   if (!token) {
+//     throw createHttpError(400, "Verification token is required");
+//   }
+
+//   const tokenHash = hashTokenVerify(token);
+
+//   const record = await EmailVerification.findOne({
+//     tokenHash,
+//     expiredAt: { $gt: Date.now() },
+//   });
+
+//   if (!record) {
+//     throw createHttpError(400, "Invalid or expired verification token");
+//   }
+
+//   const user = await User.findById(record.userId);
+//   if (!user) {
+//     throw createHttpError(404, "User not found");
+//   }
+
+//   user.emailVerified = true;
+//   await user.save();
+
+//   await EmailVerification.deleteOne({ _id: record._id });
+
+//   const session = await createSession(user._id);
+//   setSessionCookies(res, session);
+
+//   res.status(200).json({
+//     message: "Email verified successfully",
+//     user: {
+//       id: user._id,
+//       email: user.email,
+//       name: user.name,
+//     },
+//   });
+// };
 
 export const verifyEmail = async (req, res) => {
   const { token } = req.query;
@@ -129,14 +172,7 @@ export const verifyEmail = async (req, res) => {
   const session = await createSession(user._id);
   setSessionCookies(res, session);
 
-  res.status(200).json({
-    message: "Email verified successfully",
-    user: {
-      id: user._id,
-      email: user.email,
-      name: user.name,
-    },
-  });
+  return res.redirect(`${process.env.FRONTEND_URL}/calendar`);
 };
 
 
